5b9eebd8395d990336e4c23b83bb78b58875a290
AlessandroWea <atribun14@gmail.com>
2022-04-20 16:03:18 +1000

    Added Follower model and its methods

---
+++ src/models/Follower.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace app\models;
+
+use Malordo\Database\Database as Db;
+
+class Follower
+{
+    public static function follow($user_id, $followed_id)
+    {
+        Db::insert('followers', [
+            'id_user' => $user_id,
+            'id_followed' => $followed_id,
+        ]);
+    }
+
+    public static function unfollow($user_id, $followed_id)
+    {
+        Db::execute("DELETE FROM followers WHERE id_user=:id_user AND id_followed=:id_followed",[
+            'id_user' => $user_id,
+            'id_followed' => $followed_id,
+        ]);
+    }
+
+    public static function followers($id, $offset, $limit)
+    {
+        $query = Db::execute("SELECT followers.id_user as id, users.username FROM followers JOIN users ON followers.id_user = users.id WHERE followers.id_followed=:id LIMIT $offset,$limit", ['id'=>$id]);
+        return $query->fetchAll();
+    }
+
+    public static function followersCount($id)
+    {
+        $query = Db::execute("SELECT COUNT(*) FROM followers WHERE id_followed=:id", ['id'=>$id]);
+        return $query->fetch()['COUNT(*)'];
+    }
+
+    public static function followings($id, $offset, $limit)
+    {
+        $query = Db::execute("SELECT followers.id_followed as id, users.username FROM followers JOIN users ON followers.id_followed = users.id WHERE followers.id_user=:id LIMIT $offset,$limit", ['id'=>$id]);
+        return $query->fetchAll();
+    }
+
+    public static function followingsCount($id)
+    {
+        $query = Db::execute("SELECT COUNT(*) FROM followers WHERE id_user=:id", ['id'=>$id]);
+        return $query->fetch()['COUNT(*)'];
+    }
+}
